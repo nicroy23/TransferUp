ca26b43186dba7ad7ca0e29b44b6f244e60e4786
nicroy23 <62397454+nicroy23@users.noreply.github.com>
2020-08-18 13:12:56 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+app.use(express.static("public"));
+
+const crypto = require('crypto');
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  var id = crypto.randomBytes(10).toString('hex');
+  socket.join(id);
+  io.to(id).emit('new-room', id);
+  socket.on(id, (file) => {
+    io.to(id).emit('base64-file', file);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
---
+++ public/client.js
@@ -0,0 +1,43 @@
+var socket = io();
+const showId = document.querySelector('#show-room-id');
+const sendBtn = document.querySelector('#send-btn');
+const roomInput = document.querySelector('#room-input');
+var file = document.querySelector('#file-input');
+const form = document.querySelector('#file-form');
+var showImg = document.querySelector('#show-img');
+var room;
+var data;
+
+socket.on('new-room', (id) => {
+    room = id;
+    showId.innerHTML = room;
+});
+
+file.addEventListener('change', (e) => {
+    data = e.target.files[0];
+});
+
+sendBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    readThenSendFile(data);
+});
+
+function readThenSendFile(data) {
+    var reader = new FileReader();
+    reader.onload = function(evt){
+        var msg ={};
+        msg.file = evt.target.result;
+        msg.fileName = data.name;
+        console.log(msg);
+        socket.emit(roomInput.value, msg);
+    };
+    reader.readAsDataURL(data);
+}
+
+socket.on('base64-file', (file) => {
+    console.log(file.file);
+    showImg.innerHTML = `<img src="${file.file}" style="height: 280px; width: 280px;">`
+    showImg.innerHTML = `<audio controls src="${file.file}">`
+});
+
